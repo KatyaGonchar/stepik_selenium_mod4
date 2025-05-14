e3c715000ded016019b14c69456265f9abaacdf1
KatyaGonchar <katyahonchar95@gmail.com>
2025-05-14 17:41:40 +0300

    data independence
    4_3_2

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,23 +6,20 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def go_to_product_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-
-
     def add_product_to_basket(self):
-        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
-        button.click()
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
 
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
-    def should_be_correct_product_in_message(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        success_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
-        assert product_name == success_name, f"Expected '{product_name}' in success message, got '{success_name}'"
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
+    def should_be_correct_product_in_message(self, product_name):
+        added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
+        assert product_name == added_product_name, f"Expected product name '{product_name}', got '{added_product_name}'"
 
-    def should_be_correct_price_in_message(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
-        assert product_price == basket_total, f"Expected basket total '{product_price}', got '{basket_total}'"
+    def should_be_correct_price_in_message(self, product_price):
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price == basket_price, f"Expected price '{product_price}', got '{basket_price}'"
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,14 @@
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, url)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_correct_product_in_message()
-    page.should_be_correct_price_in_message()
+
+    name = page.get_product_name()
+    price = page.get_product_price()
+
+    page.should_be_correct_product_in_message(name)
+    page.should_be_correct_price_in_message(price)
